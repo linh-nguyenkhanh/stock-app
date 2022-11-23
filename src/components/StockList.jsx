0732b38581c942cc12b2a8ff429dc2ad858a58b9
Linh <linh.nguyenkhanhib@gmail.com>
2022-11-23 22:25:15 +0200

    create table with chakra UI

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 import finnHub from "../api/finnHub.js";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+
 const StockList = () => {
   const [stock, setStock] = useState();
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
-      
       try {
         const responses = await Promise.all(
           watchList.map((stock) => {
@@ -18,13 +29,13 @@ const StockList = () => {
           })
         );
 
-        console.log(responses);
-      const dataExtract = responses.map(response => {
-        return{
-          data: response.data,
-          symbol: response.config.params.symbol
-        }  
-        })
+        const dataExtract = responses.map((response) => {
+          return {
+            data: response.data,
+            symbol: response.config.params.symbol,
+          };
+        });
+        console.log(dataExtract);
         if (isMounted) {
           setStock(dataExtract);
         }
@@ -36,7 +47,40 @@ const StockList = () => {
     return () => (isMounted = false);
   }, []);
 
-  return <div>StockList</div>;
+  return (
+    <TableContainer>
+      <Table size="md" variant="striped" colorScheme="teal">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Last</Th>
+            <Th>Change</Th>
+            <Th>Change%</Th>
+            <Th>High</Th>
+            <Th>Low</Th>
+            <Th>Open</Th>
+            <Th>Pclose</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {stock?.map((stockData) => {
+            return (
+              <Tr key={stockData.symbol}>
+                <Th>{stockData.symbol}</Th>
+                <Td>{stockData.data.c}</Td>
+                <Td>{stockData.data.d}</Td>
+                <Td>{stockData.data.dp}</Td>
+                <Td>{stockData.data.h}</Td>
+                <Td>{stockData.data.l}</Td>
+                <Td>{stockData.data.o}</Td>
+                <Td>{stockData.data.pc}</Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default StockList;
