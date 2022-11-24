4a7eab2c64be46bc6afe9204189aba048be49669
Linh <linh.nguyenkhanhib@gmail.com>
2022-11-25 00:23:10 +0200

    render stock list when searching🏇

--- src/components/AutoCompletePage.jsx
+++ src/components/AutoCompletePage.jsx
@@ -1,37 +1,71 @@
-import React, { useState } from "react";
-import { Flex, FormControl, FormLabel } from "@chakra-ui/react";
-import {
-  AutoComplete,
-  AutoCompleteInput,
-  AutoCompleteItem,
-  AutoCompleteList,
-} from "@choc-ui/chakra-autocomplete";
+import React, { useState, useEffect } from "react";
+import finnHub from "../api/finnHub";
+
+import { Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
 
 const AutoCompletePage = () => {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const renderDropDown = () => {
+    const dropDownClass = search ? "show" : null;
+    return (
+      <ul className={`dropdown-menu ${dropDownClass}`}>
+        {results.map((result) => {
+          return (
+            <li className="item-dropdown" key={result.symbol}>
+              {result.description} ({result.symbol})
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search,
+          },
+        });
+        if (isMounted) {
+          setResults(response.data.result);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (search.length > 0) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+    return () => (isMounted = false);
+  }, [search]);
+
   return (
     <Flex p="10" justify="center" align="center" w="full">
       <FormControl w="60">
-        <FormLabel htmlFor="search" fontSize="md">
+        <FormLabel htmlFor="search" fontSize="lg">
           Stock Search
         </FormLabel>
-        <AutoComplete openOnFocus>
-          <AutoCompleteInput
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            variant="filled"
-            bg="rgba(145,158,171,0.4)"
-            type="text"
-            id="search"
-            placeholder="Search"
-            w="100%"
-            h="2rem"
-            p="1.2em"
-            borderRadius="6px"
-            autoComplete="off"
-          />
-          <AutoCompleteList></AutoCompleteList>
-        </AutoComplete>
+
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          variant="filled"
+          bg="#cfd1d266"
+          type="text"
+          id="search"
+          placeholder="Search"
+          w="100%"
+          h="2rem"
+          p="1.2em"
+          borderRadius="6px"
+        />
+        {renderDropDown()}
       </FormControl>
     </Flex>
   );
