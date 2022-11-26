01ed0fc036cf38e422f0886e02516f5f341c09d8
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2022-11-26 23:04:15 +0200

    fetch stock data from day, week, month and year

--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import finnHub from "../api/finnHub";
+
 const StockDetail = () => {
+  const [chartData, setCHartData] = useState();
   const { stock } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const date = new Date();
+      const currentTimeToSeconds = Math.floor(date.getTime() / 1000);
+
+      let oneDayAgo;
+      if (date.getDay() === 6) {
+        oneDayAgo = currentTimeToSeconds - 2 * 24 * 60 * 60;
+      } else if (date.getDay() === 0) {
+        oneDayAgo = currentTimeToSeconds - 3 * 24 * 60 * 60;
+      } else {
+        oneDayAgo = currentTimeToSeconds - 24 * 60 * 60;
+      }
+      const oneWeekAgo = currentTimeToSeconds - 7 * 60 * 60 * 24;
+      const oneYearAgo = currentTimeToSeconds - 365 * 60 * 60 * 24;
+      try {
+        const responses = await Promise.all([
+          finnHub.get("/stock/candle", {
+            params: {
+              stock,
+              from: oneDayAgo,
+              to: currentTimeToSeconds,
+              resolution: 30,
+            },
+          }),
+          finnHub.get("/stock/candle", {
+            params: {
+              stock,
+              from: oneWeekAgo,
+              to: currentTimeToSeconds,
+              resolution: 60,
+            },
+          }),
+          finnHub.get("/stock/candle", {
+            params: {
+              stock,
+              from: oneYearAgo,
+              to: currentTimeToSeconds,
+              resolution: "W",
+            },
+          }),
+        ]);
+        console.log(responses);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div>StockDetail {stock}</div>;
 };
 
